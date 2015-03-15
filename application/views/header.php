76adde38220122b955aa84bc12083531db0c43ed
Riaz Hasan <riaz981@gmail.com>
2015-03-16 10:34:51 +1100

    Changed the title of the page

--- application/views/header.php
+++ application/views/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Test CMS</title>
+<title>CMS</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
 <link rel="stylesheet" href="<?php echo base_url("assets/css/master.css"); ?>" />
