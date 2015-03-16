1defbd1c180f22f67af4bb11c7d5a9f0da7775a3
Riaz Hasan <riaz981@gmail.com>
2015-03-16 16:17:02 +1100

    added a lot of bootstrap classes to make the form look good

--- application/views/edit.php
+++ application/views/edit.php
@@ -19,72 +19,103 @@ $this->load->view('header');
                 <input type="text" class="form-control" id="address" name="address" placeholder="Enter address" required>
             </div>
 
+
             <div class="form-group">
                 <fieldset>
                     <legend style="color: #1489a6;">Icon informations:</legend>
-                    <label for="typeProperty">Type:</label>
-                    <input type="text" id="typeProperty" name="typeProperty" placeholder="Whole house/Just rooms" required><br>
-                    <label for="guestNumber">Number of Guests:</label>
-                    <input type="number" id="guestNumber" name="guestNumber" required><br>
-                    <label for="bedroomNumber">Number of Bedrooms:</label>
-                    <input type="number" id="bedroomNumber" name="bedroomNumber" required><br>
-                    <label for="bedsNumber">Number of Beds:</label>
-                    <input type="number" id="bedsNumber" name="bedsNumber" required><br>
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                        <label for="typeProperty">Type:</label>
+                        <input type="text" class="form-control" id="typeProperty" name="typeProperty" placeholder="Whole house/Just rooms" float:"left" required>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                            <label for="guestNumber">Number of Guests:</label>
+                            <input type="number" class="form-control" id="guestNumber" name="guestNumber" required>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6" style="margin-top:0.4em;">
+                        <label for="bedroomNumber">Number of Bedrooms:</label>
+                        <input type="number" class="form-control" id="bedroomNumber" name="bedroomNumber" required>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6" style="margin-top:0.4em;">
+                        <label for="bedsNumber">Number of Beds:</label>
+                        <input type="number" class="form-control" id="bedsNumber" name="bedsNumber" required><br>
+                    </div>
                 </fieldset>
             </div>
 
             <div class="form-group">
                 <fieldset>
                 <legend style="color: #1489a6;">Rate:</legend>
-                    <label for="nightlyRate">Nightly Price: A$</label>
-                    <input type="text" id="nightlyRate" name="nightlyRate" placeholder="Nightly Price" required><br>
-                    <label for="weeklyRate">Weekly Rate: A$</label>
-                    <input type="text" id="weeklyRate" name="weeklyRate" placeholder="Weekly Price" required><br>
-                    <label for="monthlyRate">Monthly Price: A$</label>
-                    <input type="text" id="monthlyRate" name="monthlyRate" placeholder="Monthly Price" required><br>
-                    <label for="cleaningRate">Cleaning Fee: A$</label>
-                    <input type="text" id="cleaningRate" name="cleaningRate" placeholder="Cleaning Fee" required><br>
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                        <label for="nightlyRate">Nightly Price: A$</label>
+                        <input type="text" class="form-control" id="nightlyRate" name="nightlyRate" placeholder="Nightly Price" required>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                        <label for="weeklyRate">Weekly Rate: A$</label>
+                        <input type="text" class="form-control" id="weeklyRate" name="weeklyRate" placeholder="Weekly Price" required>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6" style="margin-top:0.4em;">
+                        <label for="monthlyRate">Monthly Price: A$</label>
+                        <input type="text" class="form-control" id="monthlyRate" name="monthlyRate" placeholder="Monthly Price" required>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6" style="margin-top:0.4em;">
+                        <label for="cleaningRate">Cleaning Fee: A$</label>
+                        <input type="text" class="form-control" id="cleaningRate" name="cleaningRate" placeholder="Cleaning Fee" required>
+                    </div>
                 </fieldset>
             </div>
 
             <div class="form-group">
                 <label for="overview"><h4 style="color: #1489a6;">Overview:</h4></label>
-                <textarea class="form-control" id="overview" name="overview" placeholder="Property Overview" rows="5"></textarea>
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <textarea class="form-control" id="overview" name="overview" placeholder="Property Overview" rows="5"></textarea>
+                </div>
             </div>
 
             <fieldset>
-            <legend style="color: #1489a6;">Specifications:</legend>
+            <legend style="color: #1489a6; margin-top:0.6em;">Specifications:</legend>
                 <div class="form-group">
-                    <label for="dining">Dining:</label>
-                    <textarea class="form-control" id="dining" name="dining" placeholder="Dining" rows="3"></textarea>
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                        <label for="dining">Dining:</label>
+                        <textarea class="form-control" id="dining" name="dining" placeholder="Dining" rows="3"></textarea>
+                    </div>
+
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                        <label for="bathroomDescription">Bathroom Description:</label>
+                        <textarea class="form-control" id="bathroomDescription" name="bathroomDescription" placeholder="Bathroom Description" rows="3"></textarea>
+                    </div>
                 </div>
 
                 <div class="form-group">
-                    <label for="bathroomDescription">Bathroom Description:</label>
-                    <textarea class="form-control" id="bathroomDescription" name="bathroomDescription" placeholder="Bathroom Description" rows="3"></textarea>
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6" style="margin-top:0.4em;">
+                        <label for="bedroomDescription">Bedroom Description:</label>
+                        <textarea class="form-control" id="bedroomDescription" name="bedroomDescription" placeholder="Bedroom Description" rows="3"></textarea>
+                    </div>
+
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6" style="margin-top:0.4em;">
+                        <label for="attractionDescription">Attractions Description:</label>
+                        <textarea class="form-control" id="attractionDescription" name="attractionDescription" placeholder="Attractions Description" rows="3"></textarea>
+                    </div>
                 </div>
 
                 <div class="form-group">
-                    <label for="bedroomDescription">Bedroom Description:</label>
-                    <textarea class="form-control" id="bedroomDescription" name="bedroomDescription" placeholder="Bedroom Description" rows="3"></textarea>
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6" style="margin-top:0.4em;">
+                        <label for="leisureDescription">Leisure Activity Description:</label>
+                        <textarea class="form-control" id="leisureDescription" name="leisureDescription" placeholder="Leisure Activity Description" rows="3"></textarea>
+                    </div>
+
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6" style="margin-top:0.4em;">
+                        <label for="businessDescription">Local Services &amp Businesses Description:</label>
+                        <textarea class="form-control" id="businessDescription" name="businessDescription" placeholder="Local Service and Businesses Description" rows="3"></textarea>
+                    </div>
                 </div>
 
                 <div class="form-group">
-                    <label for="attractionDescription">Attractions Description:</label>
-                    <textarea class="form-control" id="attractionDescription" name="attractionDescription" placeholder="Attractions Description" rows="3"></textarea>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="margin-top:0.4em;">
+                        <label for="sportsDescription">Sports Adventures Activity Description:</label>
+                        <textarea class="form-control" id="sportsDescription" name="sportsDescription" placeholder="Sports Adventures Activity Description" rows="3"></textarea>
+                    </div>
                 </div>
 
-                <div class="form-group">
-                    <label for="leisureDescription">Leisure Activity Description:</label>
-                    <textarea class="form-control" id="leisureDescription" name="leisureDescription" placeholder="Leisure Activity Description" rows="3"></textarea>
-                </div>
-
-                <div class="form-group">
-                    <label for="businessDescription">Local Services and Businesses Description:</label>
-                    <textarea class="form-control" id="businessDescription" name="businessDescription" placeholder="Local Service and Businesses Description" rows="3"></textarea>
-                </div>
-
-                
             </fieldset>
         </div>
         <div class="footerForm">
