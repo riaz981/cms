f1f00f2587976615eb87b196f5dd50c64e7372ce
Riaz Hasan <riaz981@gmail.com>
2015-03-16 12:22:32 +1100

    Added class and role to the form reuired by validator for front end form validation. also i saw id and name should be side by side for the validator to work so changed that. minor indentation was also adjusted

--- application/views/login.php
+++ application/views/login.php
@@ -4,23 +4,27 @@ $this->load->view('header');
 <div class="container">
   <div class="row roundedCorner formation">
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-      <?php echo form_open('property/logincheck'); ?>
+      <?php $attributes=array('role'=>'form', 'data-toggle'=>'validator'); echo form_open('property/logincheck',$attributes); ?>
         <h3 align="center">Please Log-In</h3>
+        <div class="form-body">
         <div class="form-group">
-          <label for="firstname">Username: <span style="color:#F24B4B;">*</span></label>
-          <input type="text" class="form-control" id="username" placeholder="Enter Username" name="username"/>
+          <label for="username">Username: <span style="color:#F24B4B;">*</span></label>
+          <input type="text" class="form-control" id="username" name="username" placeholder="Enter Username" data-error="A username is required" required>
+          <div class="help-block with-errors"></div>
         </div>
         <div class="form-group">
-          <label fpr="lastname">Password: <span style="color:#F24B4B;">*</span></label>
-          <input type="password" class="form-control" id="password" placeholder="Enter Password" name="password"/>
+          <label for="password">Password: <span style="color:#F24B4B;">*</span></label>
+          <input type="password" class="form-control" id="password" name="password" placeholder="Enter Password" data-error="A password is required" required>
+          <div class="help-block with-errors"></div>
+        </div>
         </div>
         <div align="center">
-          <button type="submit" name="submit" class="btn btn-success" value="Submit">Log In</button>
+          <button type="submit" name="submit" id="submit" class="btn btn-primary">Log In</button>
           <button type="reset" class="btn btn-warning">Reset</button>
         </div>
       </form>
       <div style="margin-top:0.5em;"><span style="color:#F24B4B;"><?php if(isset($message))
-                                                                        echo $message; ?></span></div>
+                                                                            echo $message; ?></span></div>
   </div>
 </div>
 <?php
