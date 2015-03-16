49556444f1659653e330c38ac47bb3054b5b5558
Riaz Hasan <riaz981@gmail.com>
2015-03-16 12:21:07 +1100

    Added validator.js script

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,4 +1,5 @@
 <script type="text/javascript" src="<?php echo base_url("assets/js/jquery-1.11.2.js"); ?>"></script>
 <script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("assets/js/validator.js"); ?>"></script>
 </body>
 </html>
