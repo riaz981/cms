6a07016256d816fde401dacb5802d4a972b36f11
Riaz Hasan <riaz981@gmail.com>
2015-03-16 11:06:55 +1100

    Added validation if username or password is not entered. also added validation for form which checks through the database for credentials. throws back error message if not correct

--- application/controllers/property.php
+++ application/controllers/property.php
@@ -5,6 +5,7 @@ class property extends CI_Controller{
   public function __construct(){
     parent::__construct();
     $this->load->helper('url');
+    $this->load->model('propertymodel');
 
   }
 
@@ -14,15 +15,34 @@ class property extends CI_Controller{
   }
 
   public function logincheck(){
-    $firstName= $this->input->post('username');
+    $username= $this->input->post('username');
+    $password= $this->input->post('password');
 
-    if($firstName=="Riaz"){
-      echo "Hello";
+
+    //very basic validation if front end fails
+    if($username=="" || $password==""){
+        $data['message'] = "Please enter username and password!";
+        $this->load->helper(array('form'));
+        $this->load->view('login',$data);
     }
 
+    //validation of username and password
     else{
-      $this->load->helper(array('form'));
-      $this->load->view('login');
+
+        $check=$this->propertymodel->logincheck($username,$password);
+
+        //if username and password is correct
+        if($check==true){
+            echo "Welcome admin!";
+        }
+
+        //if username and password incorrect
+        else{
+
+            $data['message']="Invalid username or password. Please try again!";
+            $this->load->helper(array('form'));
+            $this->load->view('login',$data);
+        }
 
     }
   }
