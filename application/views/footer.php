1bff61590e46f9beebd6cb3ae8dbefd7c6d76863
Riaz Hasan <riaz981@gmail.com>
2015-03-17 10:01:00 +1100

    added copyright and code for the year. changes the year eg 2015-2016 in copyright if they year goes one ahead

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,5 +1,14 @@
 <script type="text/javascript" src="<?php echo base_url("assets/js/jquery-1.11.2.js"); ?>"></script>
 <script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
 <script type="text/javascript" src="<?php echo base_url("assets/js/validator.js"); ?>"></script>
+<div style="margin-top:1em;" align="center">&copy; <strong><?php $startYear=2015;
+                                                                 $thisYear=date('Y');
+                                                                 if($startYear==$thisYear){
+                                                                     echo $startYear;
+                                                                 }
+                                                                 else{
+                                                                     echo $startYear." - ".$thisYear;
+                                                                 }
+                                                            ?></strong></div>
 </body>
 </html>
