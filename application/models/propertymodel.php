c95d4c4714134cee8e1a3da00a85a54cc2cf7f48
Riaz Hasan <riaz981@gmail.com>
2015-03-18 10:39:35 +1100

    Added comments for more clarity of code

--- application/models/propertymodel.php
+++ application/models/propertymodel.php
@@ -2,6 +2,12 @@
 
 Class Propertymodel extends CI_Model{
 
+    /*
+        logincheck. gets the data from controller
+        checks if the username and password are
+        present in the database. Based on the
+        number of rows returned a validation is made
+    */
     function logincheck($username,$password){
 
         $this->db->select('ID,username,password');
@@ -21,6 +27,13 @@ Class Propertymodel extends CI_Model{
 
     }
 
+    /*
+        Insertion of data in the wp_property table
+        Takes $data as an array and inserts it
+        using the built in codeigniter function insert()
+        affected_rows shows the number of rows that were
+        affected.
+    */ 
     function insertData($data){
         $this->db->insert('wp_property',$data);
         if($this->db->affected_rows()>0){
