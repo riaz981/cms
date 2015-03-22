899a521825efc126ac5bf952f4b676d3860cc716
Riaz Hasan <riaz981@gmail.com>
2015-03-23 10:46:13 +1100

    Made a seperate navheader so that a navigation bar is present at the top

---
+++ application/views/navheader.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>CMS</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
+<link rel="stylesheet" href="<?php echo base_url("assets/css/master.css"); ?>" />
+</head>
+
+<body>
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">
+                    <img alt="Brand" src="<?php echo base_url("assets/img/as.png"); ?>">
+                </a>
+            </div>
