91dc52749784c22b3070d5c9aeff5162fd6cbc22
Riaz Hasan <riaz981@gmail.com>
2015-03-23 10:44:26 +1100

    Added part of navigation bar on this page as well changed the header from header to navheader

--- application/views/add.php
+++ application/views/add.php
@@ -1,7 +1,29 @@
 <?php
-$this->load->view('header');
+$this->load->view('navheader');
 ?>
-<div class="container">
+
+<!-- collapse the nav links, forms, and other content for toggling -->
+<div class="collapse navbar-collapse" id="bs-exaple-navbar-collapse-1">
+    <ul class="nav navbar-nav">
+        <li><a href="#">Home<span class="sr-only">(current)</span></a></li>
+        <li class="active"><a href="#">Add Property</a></li>
+    </ul>
+
+    <form class="navbar-form navbar-left" role="search">
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+    </form>
+    <ul class="nav navbar-nav navbar-right">
+        <li><a href="http://cms.localhost">Logout</a></li>
+    </ul>
+</div> <!-- navbar collapse -->
+</div>
+</nav> <!-- main navbar closes -->
+
+
+<div class="container" style="padding-top: 60px;">
   <div class="row formationadd">
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
       <div><span style="color:#F24B4B;"><?php echo validation_errors(); ?></span></div>
