c49585dc4a0b8eebfc7974341eb4d72291dc71fe
Riaz Hasan <riaz981@gmail.com>
2015-03-23 12:42:23 +1100

    changed the id in data-target to class for toggling

--- application/views/navheader.php
+++ application/views/navheader.php
@@ -13,7 +13,7 @@
             <!-- Brand toggle get grouped for better mobile display -->
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                data-target="#bs-example-navbar-collapse-1">
+                data-target=".navbar-collapse">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
