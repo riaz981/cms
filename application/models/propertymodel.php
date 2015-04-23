b522e95324cb69243c00385962b8c7eaabd84af1
Riaz Hasan <riaz981@gmail.com>
2015-04-23 15:16:23 +1000

    Indentation adjust to make it look better

--- application/models/propertymodel.php
+++ application/models/propertymodel.php
@@ -2,209 +2,188 @@
 
 Class Propertymodel extends CI_Model{
 
-    /*
-        logincheck. gets the data from controller
-        checks if the username and password are
-        present in the database. Based on the
-        number of rows returned a validation is made
-    */
-    function logincheck($username,$password){
+/*
+    logincheck. gets the data from controller
+    checks if the username and password are
+    present in the database. Based on the
+    number of rows returned a validation is made
+*/
+function logincheck($username,$password){
+    $this->db->select('ID,username,password');
+    $this->db->from('wp_credentials');
+    $this->db->where('username',$username);
+    $this->db->where('password',MD5($password));
+    $this->db->limit(1);
 
-        $this->db->select('ID,username,password');
-        $this->db->from('wp_credentials');
-        $this->db->where('username',$username);
-        $this->db->where('password',MD5($password));
-        $this->db->limit(1);
+    $query = $this->db->get();
 
-        $query = $this->db->get();
-
-        if($query->num_rows()==1){
-
+    if($query->num_rows()==1){
             $row = $query->row();
             $data = array(
                     'username' => $row->username,
                     'validated' => true
-                    );
+            );
             $this->session->set_userdata($data);
             return true;
-        }
-
-        else
-            return false;
-
     }
 
-    /*
-        Insertion of data in the wp_property table
-        Takes $data as an array and inserts it
-        using the built in codeigniter function insert()
-        affected_rows shows the number of rows that were
-        affected.
-    */
-    function insertData($data){
-        $this->db->insert('wp_property',$data);
-        if($this->db->affected_rows()>0){
-            return true;
-        }
+    else
+        return false;
+}
 
-        else{
-            return false;
-        }
-
+/*
+    Insertion of data in the wp_property table
+    Takes $data as an array and inserts it
+    using the built in codeigniter function insert()
+    affected_rows shows the number of rows that were
+    affected.
+*/
+function insertData($data){
+    $this->db->insert('wp_property',$data);
+    if($this->db->affected_rows()>0){
+        return true;
     }
-
-    function updateById($data,$id){
-        $this->db->where('id',$id);
-        $this->db->update('wp_property',$data);
-
-        if($this->db->affected_rows()>0){
-            return true;
-        }
-
-        else{
-            return false;
-        }
+    else{
+        return false;
     }
+}
 
-    function getAllPicById($id){
-        $this->db->select('id,photo_url,photo_name');
-        $this->db->where('id',$id);
-        $query = $this->db->get('wp_property');
-        if($query->num_rows()>=1){
-            return $query->result();
-        }
-        else
-         echo "Could not fetch records";
+function updateById($data,$id){
+    $this->db->where('id',$id);
+    $this->db->update('wp_property',$data);
+    if($this->db->affected_rows()>0){
+        return true;
     }
-
-    function getPicNameById($id){
-        $this->db->select('photo_name');
-        $this->db->where('id',$id);
-        $query = $this->db->get('wp_property');
-
-        if($query->num_rows()>=1){
-            $photo = array_pop($query->result());
-            return $photo->photo_name;
-        }
-        else
-         echo "Could not fetch records";
-
+    else{
+        return false;
     }
+}
 
-    function getPicUrlById($id){
-        $this->db->select('photo_url');
-        $this->db->where('id',$id);
-        $query = $this->db->get('wp_property');
-
-        if($query->num_rows()>=1){
-            $photo = array_pop($query->result());
-            return $photo->photo_url;
-        }
-        else
-         echo "Could not fetch records";
+function getAllPicById($id){
+    $this->db->select('id,photo_url,photo_name');
+    $this->db->where('id',$id);
+    $query = $this->db->get('wp_property');
+    if($query->num_rows()>=1){
+        return $query->result();
     }
+    else
+        echo "Could not fetch records";
+}
 
-    function getUploadUrlById($id){
-        $this->db->select('upload_url');
-        $this->db->where('id',$id);
-        $query = $this->db->get('wp_property');
-
-        if($query->num_rows()>=1){
-            $photo = array_pop($query->result());
-            return $photo->upload_url;
-        }
-        else
-         echo "Could not fetch records";
+function getPicNameById($id){
+    $this->db->select('photo_name');
+    $this->db->where('id',$id);
+    $query = $this->db->get('wp_property');
+    if($query->num_rows()>=1){
+        $photo = array_pop($query->result());
+        return $photo->photo_name;
     }
+    else
+        echo "Could not fetch records";
+}
 
-    function uploadPicById($id,$data){
-
-        $this->db->where('id', $id);
-        $this->db->update('wp_property', $data);
-        if($this->db->affected_rows()>0){
-            echo "Done again!";
-        }
-
-        else{
-            echo "Alas!";
-        }
-
+function getPicUrlById($id){
+    $this->db->select('photo_url');
+    $this->db->where('id',$id);
+    $query = $this->db->get('wp_property');
+    if($query->num_rows()>=1){
+        $photo = array_pop($query->result());
+        return $photo->photo_url;
     }
+    else
+        echo "Could not fetch records";
+}
 
-    /*
-        Gets all the information from the database and
-        throws information back to home function.
-    */
-    function getAll(){
-        $this->db->select('id,name,address,url');
-        $query = $this->db->get('wp_property');
-        if($query->num_rows()>=1){
-            return $query->result();
-        }
-        else
-         echo "Could not fetch records";
+function getUploadUrlById($id){
+    $this->db->select('upload_url');
+    $this->db->where('id',$id);
+    $query = $this->db->get('wp_property');
+    if($query->num_rows()>=1){
+        $photo = array_pop($query->result());
+        return $photo->upload_url;
     }
+    else
+        echo "Could not fetch records";
+}
 
-    /*
-        Gets data depending on the id passed.
-        If successful throws back the data
-        to the controller function that called
-        it.
-    */
-    function getById($id){
-        $query = $this->db->get_where('wp_property',array('id'=>$id));
-        if($query->num_rows()==1){
-            return $query->result();
-        }
-        else{
-            echo "could not fetch";
-        }
+function uploadPicById($id,$data){
+    $this->db->where('id', $id);
+    $this->db->update('wp_property', $data);
+    if($this->db->affected_rows()>0){
+        echo "Done again!";
     }
-
-    /*
-        Gets information about specific data from
-        the table. Depending on the name provided.
-        Uses like function for search for any
-        string on both sides resembling the search
-        result.
-    */
-    function getSpecific($name){
-        $this->db->like('name',$name,'both');
-        $this->db->select('id,name,address,url');
-        $query=$this->db->get('wp_property');
-
-        if($query->num_rows()>=1)
-            return $query->result();
-        else
-            echo "Could not fetch records";
+    else{
+        echo "Alas!";
     }
+}
 
-    function getIdByName($name){
-        $this->db->like('name',$name,'both');
-        $this->db->select('id');
-        $query=$this->db->get('wp_property');
-
-        if($query->num_rows()>=1){
-            $propertyId = array_pop($query->result());
-            return $propertyId->id;
-        }
-        
-        else
-            echo "Could not fetch records";
+/*
+    Gets all the information from the database and
+    throws information back to home function.
+*/
+function getAll(){
+    $this->db->select('id,name,address,url');
+    $query = $this->db->get('wp_property');
+    if($query->num_rows()>=1){
+        return $query->result();
     }
+    else
+        echo "Could not fetch records";
+}
 
-
-    function deleteRecord($id){
-        $this->db->delete('wp_property',array('id'=>$id));
-        if($this->db->affected_rows()>0){
-            return true;
-        }
-
-        else{
-            return false;
-        }
-
+/*
+    Gets data depending on the id passed.
+    If successful throws back the data
+    to the controller function that called
+    it.
+*/
+function getById($id){
+    $query = $this->db->get_where('wp_property',array('id'=>$id));
+    if($query->num_rows()==1){
+        return $query->result();
     }
+    else{
+        echo "could not fetch";
+    }
+}
 
+/*
+    Gets information about specific data from
+    the table. Depending on the name provided.
+    Uses like function for search for any
+    string on both sides resembling the search
+    result.
+*/
+function getSpecific($name){
+    $this->db->like('name',$name,'both');
+    $this->db->select('id,name,address,url');
+    $query=$this->db->get('wp_property');
+    if($query->num_rows()>=1)
+        return $query->result();
+    else
+        echo "Could not fetch records";
+}
+
+function getIdByName($name){
+    $this->db->like('name',$name,'both');
+    $this->db->select('id');
+    $query=$this->db->get('wp_property');
+    if($query->num_rows()>=1){
+        $propertyId = array_pop($query->result());
+        return $propertyId->id;
+    }
+    else
+        echo "Could not fetch records";
+}
+
+function deleteRecord($id){
+    $this->db->delete('wp_property',array('id'=>$id));
+    if($this->db->affected_rows()>0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
 
 }
